300b0fbf6f0936f5d85d009c8b9d7641b5dd2ac1
SImon Nielsen <simonnielsen@SImons-Mac.local>
2017-09-26 18:13:41 -0700

    Initial commit on fork

---
+++ Ridealong/Ridealong/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  Ridealong
+//
+//  Created by SImon Nielsen on 26/09/2017.
+//  Copyright © 2017 CSUMB. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
