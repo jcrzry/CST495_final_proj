d98233ff21caa781f5888db5ecfc3c3ae156971b
SImon Nielsen <simonnielsen@SImons-Mac.local>
2017-09-26 18:58:08 -0700

    Object models tied to the Ride object

--- Ridealong/Ridealong/FeedTableViewCell.swift
+++ Ridealong/Ridealong/FeedTableViewCell.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
 
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        super.setSelected(selected, animated:
+            animated)
 
         // Configure the view for the selected state
     }
---
+++ Ridealong/Ridealong/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Ridealong
+//
+//  Created by SImon Nielsen on 26/09/2017.
+//  Copyright © 2017 CSUMB. All rights reserved.
+//
+
+import Foundation
+struct User {
+    
+    // Properties
+    var username : String?
+    var firstName : String?
+    var lastName : String?
+    var email : String?
+    var bio : String?
+    var vehicle : Vehicle()?
+    
+    func currentRating()
+    
+}
