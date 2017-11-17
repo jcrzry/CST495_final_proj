03dd68ab25853cf6c35e2184e3cba1ad03b42f7c
Alfonso Torres <alftaurus@Alfonsos-MacBook-Pro.local>
2017-11-16 17:40:54 -0800

    merge with master

--- Ridealong/Ridealong/RealmConfig.swift
+++ Ridealong/Ridealong/RealmConfig.swift
@@ -8,11 +8,6 @@
 
 import Foundation
 import RealmSwift
-let COMMON_REALM_PATH = URL(string: "realm://ec2-54-174-95-229.compute-1.amazonaws.com:9080/CommonRealm")
-let COMMON_REALM_STRING = "http://ec2-54-174-95-229.compute-1.amazonaws.com:9080/CommonRealm"
-let SERVER_PATH = URL(string: "http://ec2-54-174-95-229.compute-1.amazonaws.com:9080")
-let PRIVATE_REALM_PATH = URL(string:"realm://ec2-54-174-95-229.compute-1.amazonaws.com:9080/~/PrivateRealm")
-
 
 func setDefaultConfiguration(realmUser: SyncUser)	{
     let defaultConfig = Realm.Configuration(syncConfiguration:SyncConfiguration(user: realmUser, realmURL: PRIVATE_REALM_PATH!), deleteRealmIfMigrationNeeded: true,objectTypes:[User.self, SimpleUser.self,Vehicle.self,Ride.self,Locations.self,Rating.self])
