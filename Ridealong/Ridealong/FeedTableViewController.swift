553d5ff2823a95af02af71bb02701a81e37abafa
JonasDeichelmann <Jonas.Deichelmann@me.com>
2017-11-28 23:41:03 -0800

    Refactoring on FeedTableViewController

--- Ridealong/Ridealong/FeedTableViewController.swift
+++ Ridealong/Ridealong/FeedTableViewController.swift
@@ -27,21 +27,13 @@ class FeedTableViewController: UITableViewController {
     //func to register cell to be dequeued by the tableview
     override func viewDidLoad() {
         super.viewDidLoad()
-
         let realm = try! Realm()
-//        let test = NSPredicate(format: "ride = %@", "title")
-//        items = (realm.objects(Ride.self).filter(test))
         items = (realm.objects(Ride.self))
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FeedCell")
 
-
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FeedCell") // [1]
-       // print(items[0].locations)
     }
-    
-    //reload the data to see the latest array fetched by allObjects()
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        tableView.reloadData()
     }
 
     //returns the number of how many cells the view should generate
@@ -52,16 +44,7 @@ class FeedTableViewController: UITableViewController {
     
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath as IndexPath) as UITableViewCell
-        
-        //let index = UInt(indexPath.row)
-        
-        //fetch object from database at current index
-        //  let ride = rides.objectAtIndex(index) as Ride
-        //        cell.textLabel?.text = items.description
         cell.textLabel?.text = "Test"
-        
         return cell
     }
-    
-    
 }
