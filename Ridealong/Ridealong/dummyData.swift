0407ee3355310e7ea93aa755dcebaaf5e41a6684
simonmartinnielsen <post.smn@gmail.com>
2017-12-31 14:43:18 +0100

    Fixing a few warnings and issues

--- Ridealong/Ridealong/dummyData.swift
+++ Ridealong/Ridealong/dummyData.swift
@@ -25,14 +25,14 @@ func AddUsers(){
             let common_config = Realm.Configuration(syncConfiguration: SyncConfiguration(user: user!, realmURL: COMMON_REALM_PATH!))
             let cRealm = try! Realm(configuration: common_config)
             for i in 0..<names.count{
-                var dummyUser = User(firstname: names[i], lastname: (names[i]+"last"), email: (names[i]+"@csumb.edu"), password: (names[i]+"password"), phone: ("123567890"))
-                var v = Vehicle(make: "Audi", model: "A4", year: 2003, type: "Sedan", isDefault: true, seats: 7)
+                let dummyUser = User(firstname: names[i], lastname: (names[i]+"last"), email: (names[i]+"@csumb.edu"), password: (names[i]+"password"), phone: ("123567890"))
+                let v = Vehicle(make: "Audi", model: "A4", year: 2003, type: "Sedan", isDefault: true, seats: 7)
                 dummyUser?.addVehicle(vehicle: v!)
                 dummyUser?.defaultVehicle = v!
                 registerUser(newUser: dummyUser!)
-                var begin = Locations(location_lat: 36.652922,location_long: -121.798163, name: "csumb")
-                var end = Locations(location_lat: 37.774929, location_long: -122.419416, name: "sf")
-                var ride = Ride(creator: dummyUser!, start: begin!, destination: end!)
+                let begin = Locations(location_lat: 36.652922,location_long: -121.798163, name: "csumb")
+                let end = Locations(location_lat: 37.774929, location_long: -122.419416, name: "sf")
+                let ride = Ride(creator: dummyUser!, start: begin!, destination: end!)
                 try! cRealm.write{
                     cRealm.create(User, value: dummyUser, update: true)
                     cRealm.create(Ride, value: ride, update: true)
